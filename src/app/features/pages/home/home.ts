3ae9e60be6e1ade89724ee2aab9a41a8e2c1733a
RyanSoar3s <ryansoares.dev@gmail.com>
2025-11-20 13:00:31 -0300

    feat: add contents component and style component

--- src/app/features/pages/home/home.ts
+++ src/app/features/pages/home/home.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Navigation } from './navigation/navigation';
-import { Header } from './header/header/header';
+import { Header } from './header/header';
+import { Contents } from './contents/contents';
+import { Responsive } from '@core/services/responsive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
+    CommonModule,
     Navigation,
-    Header
+    Header,
+    Contents
 
   ],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  protected readonly responsive = inject(Responsive);
 
 }
