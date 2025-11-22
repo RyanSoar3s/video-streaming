87adcb4c44742b925b43ea175c59114e5b4932c9
RyanSoar3s <ryansoares.dev@gmail.com>
2025-11-22 10:28:37 -0300

    feat: add locale configuration for Portuguese in app settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+  LOCALE_ID
+
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
 
 import { routes } from './app.routes';
+
+registerLocaleData(ptBr);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'pt' }
+
   ]
+  
 };
