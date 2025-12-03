23b34f35c22cb472c3a77cbe503c0e35d62bc5da
RyanSoar3s <ryansoares.dev@gmail.com>
2025-12-03 19:46:42 -0300

    feat: generate accessToken and refreshToken

---
+++ server/src/utils/generateTokens.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const ACCESS_SECRET = process.env.ACCESS_SECRET;
+const REFRESH_SECRET = process.env.REFRESH_SECRET;
+
+if (!ACCESS_SECRET || !REFRESH_SECRET) throw new Error("ACCESS_SECRET or REFRESH_SECRET not found in environment.")
+
+const ACCESS_TIME = "15m";
+const REFRESH_TIME = "7d";
+
+const generateAccessToken = (email) => jwt.sign({ email }, ACCESS_SECRET, { expiresIn: ACCESS_TIME })
+
+const generateRefreshToken = (email) => jwt.sign({ email }, REFRESH_SECRET, { expiresIn: REFRESH_TIME })
+
+export {
+  generateAccessToken,
+  generateRefreshToken
+
+}
