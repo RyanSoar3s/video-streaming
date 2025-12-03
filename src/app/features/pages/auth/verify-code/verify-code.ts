10948f24f3a071f1b4a31c1eda80d373e00682c6
RyanSoar3s <ryansoares.dev@gmail.com>
2025-12-03 19:42:54 -0300

    feat: implement verify code component with form validation and error handling

---
+++ src/app/features/pages/auth/verify-code/verify-code.ts
@@ -0,0 +1,112 @@
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, inject, viewChildren } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PendingVerification } from '@core/services/pending-verification';
+import { RequestApi } from '@core/services/request-api';
+import { Responsive } from '@core/services/responsive';
+import { timer } from 'rxjs';
+
+@Component({
+  selector: 'app-verify-code',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+
+  ],
+  templateUrl: './verify-code.html',
+  styleUrl: './verify-code.css'
+})
+export class VerifyCode implements AfterViewInit {
+  private inputs = viewChildren<ElementRef<HTMLElement>>("inputs");
+  private requestApi = inject(RequestApi);
+  private pendingVerification = inject(PendingVerification);
+  private router = inject(Router);
+  private cdr = inject(ChangeDetectorRef);
+  private fb = inject(FormBuilder);
+  protected responsive = inject(Responsive);
+
+  protected errorMsg = false;
+
+  protected form = this.fb.nonNullable.group({
+    d1: [ "", [ Validators.required, Validators.maxLength(1), Validators.pattern(/\d{1}/) ] ],
+    d2: [ "", [ Validators.required, Validators.maxLength(1), Validators.pattern(/\d{1}/) ] ],
+    d3: [ "", [ Validators.required, Validators.maxLength(1), Validators.pattern(/\d{1}/) ] ],
+    d4: [ "", [ Validators.required, Validators.maxLength(1), Validators.pattern(/\d{1}/) ] ],
+    d5: [ "", [ Validators.required, Validators.maxLength(1), Validators.pattern(/\d{1}/) ] ],
+    d6: [ "", [ Validators.required, Validators.maxLength(1), Validators.pattern(/\d{1}/) ] ]
+
+  });
+
+  protected isDigit = true;
+
+  protected lastKey: string = "";
+
+  ngAfterViewInit(): void {
+    this.inputs()[0].nativeElement.focus();
+
+    this.form.valueChanges.subscribe((values) => {
+      const allDigits = Object.values(values).every((v) => v === "" || /^\d$/.test(v));
+
+      this.isDigit = allDigits;
+
+    });
+
+  }
+
+  selectNextInput(index: number): void {
+    if (!/^[a-zA-Z0-9]$/.test(this.lastKey)) return;
+
+    let i = (index <= 5 && index + 1 <= 5) ? index + 1 : 0;
+    let verifieldInputs = 0;
+
+    while (verifieldInputs <= 5) {
+      const input = this.inputs()[i++].nativeElement as HTMLInputElement;
+
+      if (!input.value) {
+        input.focus();
+        break;
+
+      }
+
+      if (i > 5) i = 0;
+
+      verifieldInputs++;
+
+    }
+
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      const values = Object.values(this.form.value);
+      const email = this.pendingVerification.getEmail();
+
+      this.requestApi.verify(email, values.join("")).subscribe({
+        next: (value) => {
+          console.log(value.message);
+          this.pendingVerification.clear();
+          this.router.navigate([ "/" ]);
+
+        },
+        error: (error) => {
+          this.errorMsg = true;
+          console.error(`Error: ${error}`);
+          this.form.reset();
+          this.cdr.detectChanges();
+
+          timer(1000).subscribe(() => {
+            this.errorMsg = false;
+            this.cdr.detectChanges();
+
+          });
+
+        }
+
+      });
+
+    }
+
+  }
+
+}
