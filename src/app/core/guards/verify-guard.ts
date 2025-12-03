5c3d889fc4da8d86086728057b3905601f9a5ae8
RyanSoar3s <ryansoares.dev@gmail.com>
2025-12-03 19:39:39 -0300

    feat: create a guard to check for pending validation

---
+++ src/app/core/guards/verify-guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core/primitives/di';
+import { CanActivateFn, Router } from '@angular/router';
+import { PendingVerification } from '@core/services/pending-verification';
+
+export const verifyGuard: CanActivateFn = () => {
+  const pendingVerification = inject(PendingVerification);
+  const isPendingVerification = pendingVerification.isPendingVerification();
+  const router = inject(Router);
+
+  if (!isPendingVerification) {
+    router.navigate([ "" ]);
+    return false;
+
+  }
+
+  return true;
+};
