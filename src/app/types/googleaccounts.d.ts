dc6a2152937fc7cee40529697000b0a9a7e2a521
RyanSoar3s <ryansoares.dev@gmail.com>
2025-12-03 19:28:17 -0300

    feat: add typing to google namespace

---
+++ src/app/types/googleaccounts.d.ts
@@ -0,0 +1,9 @@
+declare namespace google {
+  export namespace accounts.id {
+    function initialize(config: any): void;
+    function renderButton(element: HTMLElement, options: any): void;
+    function prompt(): void;
+
+  }
+
+}
