5b7d484737960fb277411d652d48b33644d361b0
RyanSoar3s <ryansoares.dev@gmail.com>
2025-12-03 19:49:02 -0300

    feat: create the MongoDB Schema

---
+++ server/src/db/user.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  hashPass: { type: String },
+  verificationCode: { type: String },
+  expiresAt: { type: Date, index: { expires: 0 } },
+  verified: { type: Boolean, required: true },
+  refreshTokens: [ { token: String, createAt: { type: Date, default: Date.now } } ]
+
+}, { timestamps: true })
+
+export default mongoose.model("users", userSchema)
