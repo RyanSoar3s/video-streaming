85b27a682156c1a3b0ac32d5805cb1631cf4d6e4
RyanSoar3s <ryansoares.dev@gmail.com>
2025-12-08 14:03:23 -0300

    refactor: refactor the method to get oldUsername from injected service

--- src/app/features/pages/home/profile/validators/validateFormField.ts
+++ src/app/features/pages/home/profile/validators/validateFormField.ts
@@ -1,19 +1,26 @@
-import { AbstractControl, ValidationErrors } from "@angular/forms";
+import { inject } from "@angular/core";
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+import { ProfileInfo } from "@core/services/profile-info";
 
-const validateUsername = (oldUsername: string) => {
+const validateUsername = (): ValidatorFn => {
+  const profileInfo = inject(ProfileInfo);
+
   return (control: AbstractControl): ValidationErrors | null => {
-    const username = (control.value ?? "").trim();
+    const oldUsername = profileInfo.profileInfo().username;
+    const username = control.get("username")?.value ?? "";
 
     return (
-            username !== "" &&
-            username.length > 4 &&
-            oldUsername !== username
-
-          ) ? null : { invalidUsername: true };
+      username.trim() !== "" &&
+      username.trim().length > 4 &&
+      oldUsername !== username.trim()
+    )
+      ? null
+      : { invalidUsername: true };
 
   };
 
-};
+}
+
 
 const validatePassword = (control: AbstractControl): ValidationErrors | null => {
   const password = control.get("password")?.value.trim();
