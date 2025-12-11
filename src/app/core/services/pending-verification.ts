a27c5a68592c31aaecbbb62a4fa7a81fc178f2e5
RyanSoar3s <ryansoares.dev@gmail.com>
2025-12-11 11:54:29 -0300

    refactor: add email field to localStorage

--- src/app/core/services/pending-verification.ts
+++ src/app/core/services/pending-verification.ts
@@ -5,8 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class PendingVerification {
   private STORAGE_FLAG = "pending_verification";
+  private EMAIL_FLAG = "email";
+
   setPendingVerification(value: boolean): void {
     localStorage.setItem(this.STORAGE_FLAG, (value) ? "true" : "false");
+
+  }
+
+  setEmail(email: string): void {
+    localStorage.setItem(this.EMAIL_FLAG, email);
+
+  }
+
+  getEmail(): string {
+    return localStorage.getItem(this.EMAIL_FLAG) as string;
 
   }
 
@@ -17,6 +29,7 @@ export class PendingVerification {
 
   clear(): void {
     localStorage.removeItem(this.STORAGE_FLAG);
+    localStorage.removeItem(this.EMAIL_FLAG);
 
   }
 
