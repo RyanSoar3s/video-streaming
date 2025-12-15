071cc381993c1542c6b13bf5dd62ede7aa9c6491
RyanSoar3s <ryansoares.dev@gmail.com>
2025-12-15 11:33:24 -0300

    feat: create mocks for register/login errors

---
+++ src/app/mocks/errors/request-api.error.mock.ts
@@ -0,0 +1,53 @@
+import { responseError } from "@core/models/responseError.model";
+import { RequestApi } from "@core/services/request-api";
+import { throwError } from "rxjs";
+import { vi } from "vitest";
+
+const accessGoogleErrorMock = (request: RequestApi) => {
+  vi.spyOn(request, "accessGoogle").mockReturnValue(
+    throwError(() => ({
+      error: {
+        message: "Token inválido"
+
+      }
+
+    } satisfies responseError))
+
+  );
+
+};
+
+const registerErrorMock = (request: RequestApi) => {
+  vi.spyOn(request, "register").mockReturnValue(
+    throwError(() => ({
+      error: {
+        message: "O usuário já existe"
+
+      }
+
+    } satisfies responseError))
+
+  );
+
+};
+
+const loginErrorMock = (request: RequestApi) => {
+  vi.spyOn(request, "login").mockReturnValue(
+    throwError(() => ({
+      error: {
+        message: "Usuário não existe"
+
+      }
+
+    } satisfies responseError))
+
+  );
+
+};
+
+export {
+  accessGoogleErrorMock,
+  registerErrorMock,
+  loginErrorMock
+
+};
