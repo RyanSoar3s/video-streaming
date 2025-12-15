f4c0e414d3c38ec54d0025556dbf4af3584f63f8
RyanSoar3s <ryansoares.dev@gmail.com>
2025-12-15 11:22:32 -0300

    test: add locale config

---
+++ setup.locale.ts
@@ -0,0 +1,7 @@
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+
+const setupLocale = () => registerLocaleData(pt, 'pt-BR');
+
+export default setupLocale;
+
