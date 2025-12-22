34a8ec77f468194c5c9f1dd8c86fda3a6dc41426
RyanSoar3s <ryansoares.dev@gmail.com>
2025-12-22 10:02:48 -0300

    test: add data-testid to profile link and update test for navigation

--- src/app/features/pages/home/header/header.spec.ts
+++ src/app/features/pages/home/header/header.spec.ts
@@ -1,16 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
 import { expect, it, describe, beforeEach } from 'vitest';
 
 import { Header } from './header'
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, provideRouter, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { Profile } from '../profile/profile';
+import { screen } from '@testing-library/angular';
+import { userEvent } from '@testing-library/user-event';
 
 describe('Header', () => {
-  let component: Header
-  let fixture: ComponentFixture<Header>
+  let component: Header;
+  let fixture: ComponentFixture<Header>;
+  let location: Location;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ Header ],
+      imports: [
+        Header,
+        Profile
+
+      ],
       providers: [
         {
           provide: ActivatedRoute,
@@ -18,17 +28,44 @@ describe('Header', () => {
             snapshot: { params: {}, queryParams: {} }
           }
 
-        }
+        },
+        Router,
+        Location,
+        provideRouter([
+          {
+            path: "home",
+              children: [
+                {
+                  path: "profile",
+                  component: Profile
+
+                }
+
+            ]
+
+          }
+
+        ])
 
       ]
-    }).compileComponents()
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(Header)
-    component = fixture.componentInstance
-    fixture.detectChanges()
+    location = TestBed.inject(Location);
+    router = TestBed.inject(Router);
+    fixture = TestBed.createComponent(Header);
+    component = fixture.componentInstance;
+    router.initialNavigation();
+    fixture.detectChanges();
   })
 
-  it('should create', () => {
-    expect(component).toBeTruthy()
+  it('should open the route profile', async () => {
+    const routerLink = screen.getByTestId("profile-link");
+
+    const user = userEvent.setup();
+
+    await user.click(routerLink);
+
+    expect(location.path()).toBe("/home/profile");
+
   })
 })
