e76bc636c47576f4568c737f345ea6c878fa16b5
RyanSoar3s <ryansoares.dev@gmail.com>
2025-12-22 10:03:21 -0300

    refactor: remove unused component fixture and streamline profile information tests

--- src/app/features/pages/home/home.spec.ts
+++ src/app/features/pages/home/home.spec.ts
@@ -1,14 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { expect, it, describe, beforeEach, beforeAll } from 'vitest';
 
 import { Home } from './home';
 import { ActivatedRoute } from '@angular/router';
 import setupLocale from "@locale"
+import { RequestApi } from '@core/services/request-api';
+import { ProfileInfo } from '@core/services/profile-info';
+import { HttpClient } from '@angular/common/http';
+import { httpClientProfileMock } from '@mock/httpclient.mock';
+import { httpClientProfileErrorMock } from '@mock/errors/httpclient.error.mock';
+import { responseError } from '@core/models/responseError.model';
 
 describe('Home', () => {
-  let component: Home;
-  let fixture: ComponentFixture<Home>;
-
+  let request: RequestApi;
+  let profileInfo: ProfileInfo;
+  let http: HttpClient;
 
   beforeAll(() => {
     setupLocale();
@@ -24,19 +30,44 @@ describe('Home', () => {
             snapshot: { params: {}, queryParams: {} }
           }
 
-        }
+        },
+        RequestApi,
+        ProfileInfo,
+        HttpClient
 
       ]
 
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Home);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    request = TestBed.inject(RequestApi);
+    profileInfo = TestBed.inject(ProfileInfo);
+    http = TestBed.inject(HttpClient);
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should obtain profile information', () => {
+    httpClientProfileMock(http);
+    request.profile().subscribe();
+
+    expect(profileInfo.profileInfo().email).toBe("test@email.com");
+    expect(profileInfo.profileInfo().username).toBe("user10");
+
   });
+
+  it('should throw error when obtain profile information', () => {
+    expect.assertions(1);
+
+    httpClientProfileErrorMock(http);
+
+    request.profile().subscribe({
+      error: (error: responseError) => {
+        expect(error.error.message).toBe("Refresh token está ausente");
+
+      }
+
+    });
+
+  });
+
 });
