ec1ca33f337528486a2e0d222eb52a299c4d5f04
RyanSoar3s <ryansoares.dev@gmail.com>
2026-01-01 16:15:43 -0300

    feat: implement service to store catalog

---
+++ src/app/core/services/video-streaming.ts
@@ -0,0 +1,45 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { TContent, TVideoStreaming } from '@models/videoStreaming.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VideoStreaming {
+  private videoStreamingSignal = signal<TVideoStreaming | null>(null);
+  public videoStreaming = computed(() => this.videoStreamingSignal());
+
+  public allFiltered = computed(() => {
+    const vs = this.videoStreamingSignal();
+    if (!vs) return null;
+
+    return [
+      this.processSection(vs.MostWatched, "rating"),
+      this.processSection(vs.Releases, "year"),
+      this.processSection(vs.Movies),
+      this.processSection(vs.Series),
+      this.processSection(vs.Animations)
+
+    ];
+
+  });
+
+  private processSection(section: TContent, sortKey?: string, amount = 15): TContent {
+    let items = [ ...section.items ];
+
+    if (sortKey === "rating") items.sort((a, b) => b.rating - a.rating);
+    if (sortKey === "year") items.sort((a, b) => b.year - a.year);
+
+    return {
+      sectionTitle: section.sectionTitle,
+      items: items.slice(0, amount)
+
+    } satisfies TContent;
+
+  }
+
+  setVideoStreamingContent(content: TVideoStreaming): void {
+    this.videoStreamingSignal.set(content);
+
+  }
+
+}
