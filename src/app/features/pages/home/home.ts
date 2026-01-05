dc78fb077e9cf68eba9133c6f5d6a1c8d542c752
RyanSoar3s <ryansoares.dev@gmail.com>
2026-01-05 10:39:11 -0300

    refactor: remove app-contents and add router-outlet

--- src/app/features/pages/home/home.ts
+++ src/app/features/pages/home/home.ts
@@ -1,11 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Navigation } from './navigation/navigation';
 import { Header } from './header/header';
-import { Contents } from './contents/contents';
 import { Responsive } from '@core/services/responsive';
 import { CommonModule } from '@angular/common';
 import { RequestApi } from '@core/services/request-api';
 import { responseError } from '@core/models/responseError.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +13,7 @@ import { responseError } from '@core/models/responseError.model';
     CommonModule,
     Navigation,
     Header,
-    Contents
+    RouterModule
 
   ],
   templateUrl: './home.html',
