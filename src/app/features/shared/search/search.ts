272288233571af9d631ddc76b41393583b361907
RyanSoar3s <ryansoares.dev@gmail.com>
2026-01-05 10:47:47 -0300

    feat: create search component to find searched content

---
+++ src/app/features/shared/search/search.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, inject, input, output, viewChild } from '@angular/core';
+import { Responsive } from '@core/services/responsive';
+import { ɵInternalFormsSharedModule } from "@angular/forms";
+import { VideoStreaming } from '@core/services/video-streaming';
+import { TContent } from '@models/videoStreaming.model';
+
+@Component({
+  selector: 'app-search',
+  imports: [
+    CommonModule,
+    ɵInternalFormsSharedModule
+],
+  templateUrl: './search.html',
+  styleUrl: './search.css',
+  host: {
+    '[style.height]': `height() + 'px'`,
+    '[style.width]': `width() + '%'`,
+    '[style.margin]': 'margin()'
+
+  }
+})
+export class Search {
+  private search = viewChild<ElementRef<HTMLInputElement>>("search");
+  private videoStreaming = inject(VideoStreaming);
+  protected readonly responsive = inject(Responsive);
+
+  protected readonly magnifyingGlass = "assets/home/magnifying-glass-white.png";
+
+  height = input.required<number>();
+  width = input.required<number>();
+  margin = input<string>("0px");
+  borderRadius = input<string>("0px");
+
+  content = output<Array<TContent> | null>();
+
+  searchContent(): void {
+    const search = this.search()?.nativeElement.value;
+    const searchContent = this.videoStreaming.searchByTitle(search!);
+
+    this.content.emit(
+                      [
+                        {
+                          sectionTitle: `Resultado: ${search}`,
+                          items: searchContent!
+
+                        }
+
+                      ] satisfies Array<TContent>
+
+                    );
+
+  }
+
+}
