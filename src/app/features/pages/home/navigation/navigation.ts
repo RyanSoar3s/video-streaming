88779de8cda74fe47a30fbbde774a0447a75c1af
RyanSoar3s <ryansoares.dev@gmail.com>
2026-01-07 17:31:34 -0300

    refactor: enhance navigation links with query parameters and options

--- src/app/features/pages/home/navigation/navigation.ts
+++ src/app/features/pages/home/navigation/navigation.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Responsive } from '@core/services/responsive';
+import { VideoStreaming } from '@core/services/video-streaming';
+import { TVideoStreaming, TContent } from '@models/videoStreaming.model';
 
 @Component({
   selector: 'app-navigation',
@@ -14,16 +16,18 @@ import { Responsive } from '@core/services/responsive';
   styleUrl: './navigation.css'
 })
 export class Navigation {
+  protected readonly videoStreaming = inject(VideoStreaming);
+
   protected readonly bars = "assets/navigation/bars.png";
   protected readonly xMark = "assets/navigation/x-mark.png";
   protected readonly magnifyingGlass = "assets/home/magnifying-glass-white.png";
 
   protected readonly icons = [
-    { id: 0, name: "Início",      icon: "assets/navigation/house.png", link: "" },
-    { id: 1, name: "Catálogo",    icon: "assets/navigation/tv.png",    link: "catalog" },
-    { id: 2, name: "Mais vistos", icon: "assets/navigation/fire.png",  link: "" },
-    { id: 3, name: "Favoritos",   icon: "assets/navigation/star.png",  link: "" },
-    { id: 4, name: "Biblioteca",  icon: "assets/navigation/layer.png", link: "" }
+    { id: 0, name: "Início",      icon: "assets/navigation/house.png", option: [ "", "" ],                        link: "" },
+    { id: 1, name: "Catálogo",    icon: "assets/navigation/tv.png",    option: [ "Todos", "All" ],                link: "catalog" },
+    { id: 2, name: "Mais vistos", icon: "assets/navigation/fire.png",  option: [ "Mais Vistos", "MostWatched" ],  link: "catalog" },
+    { id: 3, name: "Favoritos",   icon: "assets/navigation/star.png",  option: [ "", "" ],                        link: "" },
+    { id: 4, name: "Biblioteca",  icon: "assets/navigation/layer.png", option: [ "", "" ],                        link: "" }
 
   ];
 
@@ -37,16 +41,25 @@ export class Navigation {
   ];
 
   protected readonly trendingContent = [
-    { id: 0, name: "Ação", color: "bg-blue-500" },
-    { id: 1, name: "Aventura", color: "bg-orange-500" },
-    { id: 2, name: "Comédia", color: "bg-red-500" },
-    { id: 3, name: "Terror", color: "bg-yellow-500" },
-    { id: 4, name: "Fantasia", color: "bg-green-500" },
-    { id: 5, name: "Animação", color: "bg-pink-500" }
+    { id: 0, name: "Ação",              color: "bg-red-600" },
+    { id: 1, name: "Aventura",          color: "bg-emerald-600" },
+    { id: 2, name: "Comédia",           color: "bg-yellow-400" },
+    { id: 3, name: "Terror",            color: "bg-gray-600" },
+    { id: 4, name: "Ficção Científica", color: "bg-purple-600" },
+    { id: 5, name: "Animação",          color: "bg-sky-400" }
 
   ];
 
   protected readonly responsive = inject(Responsive);
   public isOpen = false;
+
+  getCatalog(option: Array<string>): Array<{ param: string } & TContent> {
+    const vs = this.videoStreaming.videoStreaming();
+    const key = option[1] as keyof TVideoStreaming;
+    const content = { param: option[0], ...vs![key] };
+
+    return [ content ] satisfies Array<{ param: string } & TContent>;
+
+  }
 
 }
