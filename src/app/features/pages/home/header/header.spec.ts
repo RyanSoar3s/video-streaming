4802eea5017951d20e3efee5a0b23d446eef7634
RyanSoar3s <ryansoares.dev@gmail.com>
2026-01-13 10:40:16 -0300

    test: add test to open open profile and access banner content

--- src/app/features/pages/home/header/header.spec.ts
+++ src/app/features/pages/home/header/header.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { expect, it, describe, beforeEach } from 'vitest';
 
 import { Header } from './header'
@@ -7,12 +7,15 @@ import { Location } from '@angular/common';
 import { Profile } from '../profile/profile';
 import { screen } from '@testing-library/angular';
 import { userEvent } from '@testing-library/user-event';
+import { VideoStreaming } from '@core/services/video-streaming';
+import videoStreamingMock from '@mock/video-streaming.mock';
+import { profileGuard } from '@core/guards/profile-guard';
+import { contentAccessGuard } from '@core/guards/content-access-guard';
+import { ContentPage } from '@features/shared/content-page/content-page';
 
 describe('Header', () => {
-  let component: Header;
   let fixture: ComponentFixture<Header>;
   let location: Location;
-  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,24 +25,28 @@ describe('Header', () => {
 
       ],
       providers: [
+        { provide: VideoStreaming, useValue: videoStreamingMock },
+        { provide: profileGuard, useValue: () => true },
+        { provide: contentAccessGuard, useValue: () => true },
         {
           provide: ActivatedRoute,
-          useValue: {
-            snapshot: { params: {}, queryParams: {} }
-          }
-
+          useValue: { snapshot: { params: {}, queryParams: {} } }
         },
-        Router,
-        Location,
         provideRouter([
           {
             path: "home",
-              children: [
-                {
-                  path: "profile",
-                  component: Profile
+            children: [
+              {
+                path: "profile",
+                component: Profile,
+                outlet: "info"
 
-                }
+              },
+              {
+                path: "content",
+                component: ContentPage
+
+              }
 
             ]
 
@@ -48,24 +55,36 @@ describe('Header', () => {
         ])
 
       ]
+
     }).compileComponents();
 
     location = TestBed.inject(Location);
-    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(Header);
-    component = fixture.componentInstance;
-    router.initialNavigation();
-    fixture.detectChanges();
-  })
 
-  it('should open the route profile', async () => {
+    await fixture.whenStable();
+
+  });
+  it("should open the route profile", async () => {
     const routerLink = screen.getByTestId("profile-link");
 
     const user = userEvent.setup();
 
     await user.click(routerLink);
 
-    expect(location.path()).toBe("/home/profile");
+    expect(location.path()).toBe("/home/(info:profile)");
 
   })
+
+  it("should show banner content", async () => {
+    const user = userEvent.setup();
+
+    await user.click(
+      screen.getByRole("button", { name: "Assistir" })
+
+    );
+
+    expect(location.path()).toBe("/home/content?title=Sherlock");
+
+  });
+
 })
